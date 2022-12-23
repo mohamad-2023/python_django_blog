cc5e1a0a0da3fb96ab3153d07a256a326b44b92d
mohammad-2023 <mmm19870linkin@gmail.com>
2022-12-23 08:31:12 +0100

    add posts html

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 from .models import Post
 
 
 
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+    
+
+admin.site.register(Post,PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=10000)
     image=models.ImageField(upload_to='posts/')
-    #author
-    #tags
+    author= models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+    tags = TaggableManager()
 
 
 
